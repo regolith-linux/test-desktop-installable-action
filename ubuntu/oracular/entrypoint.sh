6fc9d3328fb5303526e4de38cfc60049e7e821d3
Regolith Linux <regolith.linux@gmail.com>
2024-10-19 16:11:21 -0700

    feat: add oracular to test configuration

---
+++ ubuntu/oracular/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+set -e
+
+APT_KEY_URL=$1
+APT_REPO_LINE=$2
+TARGET_PACKAGE=$3
+
+apt update
+
+# Install repo key
+wget -qO - "$APT_KEY_URL" | apt-key add -
+
+# Update apt configuration with new repository
+echo "$APT_REPO_LINE" | tee /etc/apt/sources.list.d/regolith.list
+
+# Install target package
+apt update
+# shellcheck disable=SC2086
+DEBIAN_FRONTEND=noninteractive apt install -y $TARGET_PACKAGE
